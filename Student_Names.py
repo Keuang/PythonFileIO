a76963f47427280930957e93fca4f91a38ccbe8f
Mingi Hong <keuang@wireless-10-146-202-162.public.utexas.edu>
2023-01-24 17:48:55 -0600

    student name py changed

--- Student_Names.py
+++ Student_Names.py
@@ -1,8 +1,21 @@
-s = open('studentnames.txt', 'r')
-print(s.readline().rstrip('\n'))
-print(s.readline().rstrip('\n'))
-print(s.readline().rstrip('\n'))
-print(s.readline().rstrip('\n'))
-print(s.readline().rstrip('\n'))
-print(s.readline().rstrip('\n'))
-s.close()
+# s = open('studentnames.txt', 'r')
+# print(s.readline().rstrip('\n'))
+# print(s.readline().rstrip('\n'))
+# print(s.readline().rstrip('\n'))
+# print(s.readline().rstrip('\n'))
+# print(s.readline().rstrip('\n'))
+# print(s.readline().rstrip('\n'))
+# s.close()
+
+
+studentData = open('studentnames.txt','r')
+count = 0
+for student in studentData:
+    name = student.rstrip("\n")
+    print(name)
+    count += 1
+print(count)
+studentData.close()
+
+
+
